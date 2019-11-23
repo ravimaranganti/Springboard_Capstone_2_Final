49cab650e7d9f4822ffd9576727c019c2fc0a97d
ravimaranganti <48893679+ravimaranganti@users.noreply.github.com>
2019-11-22 22:36:00 -0600

    Add files via upload

---
+++ input_generation/audio_preprocess.py
@@ -0,0 +1,41 @@
+L=16000
+import os
+import numpy as np
+from scipy.fftpack import fft
+from scipy.io import wavfile
+from scipy import signal
+from glob import glob
+import re
+import pandas as pd
+import gc
+from scipy.io import wavfile
+def log_specgram(audio, sample_rate, window_size=20,
+                 step_size=10, eps=1e-10):
+    nperseg = int(round(window_size * sample_rate / 1e3))
+    noverlap = int(round(step_size * sample_rate / 1e3))
+    freqs, times, spec = signal.spectrogram(audio,
+                                    fs=sample_rate,
+                                    window='hann',
+                                    nperseg=nperseg,
+                                    noverlap=noverlap,
+                                    detrend=False)
+    return freqs, times, np.log(spec.T.astype(np.float32) + eps)
+def pad_audio(samples):
+    if len(samples) >= L: return samples
+    else: return np.pad(samples, pad_width=(L - len(samples), 0), mode='constant', constant_values=(0, 0))
+
+def chop_audio(samples, L=16000, num=1000):
+    for i in range(num):
+        beg = np.random.randint(0, len(samples) - L)
+        yield samples[beg: beg + L]
+
+def label_transform(labels):
+    nlabels = []
+    for label in labels:
+        if label == '_background_noise_':
+            nlabels.append('silence')
+        elif label not in legal_labels:
+            nlabels.append('unknown')
+        else:
+            nlabels.append(label)
+    return pd.get_dummies(pd.Series(nlabels))
